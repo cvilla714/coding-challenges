d5fbbd14c0524bb938dd2413b6287d5894e50349
Cosmel Villalobos <cosmelvg@gmail.com>
2021-02-18 12:58:14 -0500

    adding explanation of tournament winner code

--- tournamentwinner.js
+++ tournamentwinner.js
@@ -12,23 +12,31 @@ function tournamentWinner(competitions, results) {
   //   start the for loop , it will run as long as the indx is less than the lengthe of the competition and int will increment by one
   // on every iteration
   for (let idx = 0; idx < competitions.length; idx++) {
-    //
+    //this will tell us if the home team of away team won
     const result = results[idx];
+    // this is how we are decomposing the hometeam and away team
     const [hometeam, awayteam] = competitions[idx];
-
+    // here we are using the ternary operator where is the winnig team is equals to the result of the home__team_winning otherwise
+    // the away team won
     const winningTeam = result === home_team_won ? hometeam : awayteam;
-
+    // this function will update the scores adding 3 pts to the winner
     updateScores(winningTeam, 3, scores);
 
+    // we make the condition to switch the score to the corresponding team like this
+    //if the score of the winning team is greater than the currentbestteam then
+    // the currentbestTeam will be the winning team score
     if (scores[winningTeam] > scores[currentBestTeam]) {
       currentBestTeam = winningTeam;
     }
   }
+  // here i am returning the currentBestTeam
   return currentBestTeam;
 }
+// we are taking 3 parametes . the team , the points we have to add and the score we need to update
 function updateScores(team, points, scores) {
+  // if the team is not in the scores tables then ijust have to add it
   if (!(team in scores)) scores[team] = 0;
-
+  // and update the score of the team
   scores[team] += points;
 }
 console.log(
